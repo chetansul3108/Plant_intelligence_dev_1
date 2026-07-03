ac55c9fd8127b1071121717e2b777cce0a7338a8
chetan sul <chetan.sul@yash.com>
2026-07-03 09:59:48 +0000

    added yash logo and UI changes

--- app/plantitelliapp/webapp/formatter/kpiformatter.js
+++ app/plantitelliapp/webapp/formatter/kpiformatter.js
@@ -2,18 +2,18 @@ sap.ui.define([
     "sap/ui/core/library"
 ], function (coreLibrary) {
     "use strict";
-
+ 
     var ValueState = coreLibrary.ValueState;
-
+ 
     function toNumber(v) {
         var n = parseFloat(v);
         return isNaN(n) ? 0 : n;
     }
-
+ 
     return {
         getTargetStatusText: function (sKey, vValue) {
             var nValue = toNumber(vValue);
-
+ 
             switch (sKey) {
                 case "onTimeDelivery":
                 case "otif":
@@ -24,7 +24,7 @@ sap.ui.define([
                         return "MONITOR";
                     }
                     return "CRITICAL";
-
+ 
                 case "orderLifecycle":
                     if (nValue <= 5) {
                         return "ON TRACK";
@@ -33,10 +33,10 @@ sap.ui.define([
                         return "MONITOR";
                     }
                     return "CRITICAL";
-
+ 
                 case "stockShortage":
                     return nValue > 0 ? "CRITICAL" : "ON TRACK";
-
+ 
                 case "scheduleRisk":
                     if (nValue === 0) {
                         return "ON TRACK";
@@ -45,18 +45,18 @@ sap.ui.define([
                         return "MONITOR";
                     }
                     return "CRITICAL";
-
+ 
                 case "transitRisk":
                     return nValue > 0 ? "WATCH" : "ON TRACK";
-
+ 
                 default:
                     return "MONITOR";
             }
         },
-
+ 
         getTargetStatusState: function (sKey, vValue) {
             var nValue = toNumber(vValue);
-
+ 
             switch (sKey) {
                 case "onTimeDelivery":
                 case "otif":
@@ -67,7 +67,7 @@ sap.ui.define([
                         return ValueState.Warning;
                     }
                     return ValueState.Error;
-
+ 
                 case "orderLifecycle":
                     if (nValue <= 5) {
                         return ValueState.Success;
@@ -76,10 +76,10 @@ sap.ui.define([
                         return ValueState.Warning;
                     }
                     return ValueState.Error;
-
+ 
                 case "stockShortage":
                     return nValue > 0 ? ValueState.Error : ValueState.Success;
-
+ 
                 case "scheduleRisk":
                     if (nValue === 0) {
                         return ValueState.Success;
@@ -88,13 +88,14 @@ sap.ui.define([
                         return ValueState.Warning;
                     }
                     return ValueState.Error;
-
+ 
                 case "transitRisk":
                     return nValue > 0 ? ValueState.Information : ValueState.Success;
-
+ 
                 default:
                     return ValueState.None;
             }
         }
     };
 });
+ 
